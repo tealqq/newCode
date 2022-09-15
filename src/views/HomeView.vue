123bc31a39698ee10bd14e5fb5be433683932bd2
tealqq <tealplayer1@gmail.com>
2022-09-14 21:19:36 -0400

    init commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <login />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import login from '@/components/login.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    login,
   },
 });
 </script>
